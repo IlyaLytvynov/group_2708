08d7d7a10758a62fb0a7cc46f23f5a9f0a32d898
Illia Lytvynov <ilyaillych@gmail.com>
2019-10-05 15:30:06 +0300

    Added lesson 10

---
+++ src/lesson_10/lesson_10.js
@@ -0,0 +1,19 @@
+import './lesson_10.scss';
+const ADMIN_USER_NAME = 'admin';
+const SUPER_USER = 'Ilya';
+let username = prompt('Enter name');
+
+if (username === ADMIN_USER_NAME) {
+  header.innerHTML = 'Hello master!';
+  header.style.background = 'yellow';
+  header.style.color = 'blue';
+  header.title = 'MASTER TOOLTIP';
+  header.className = 'test dark';
+} else if (username === SUPER_USER) {
+  header.innerHTML = 'HELLO SUPER USER';
+  header.style.background = 'green';
+  header.style.color = 'aqua';
+} else {
+  header.innerHTML = 'Hello guest!';
+  header.style.background = 'red';
+}
